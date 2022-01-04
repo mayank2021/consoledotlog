78e225b4fd8326224a6533c1bf442d4283373d0f
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2022-01-04 14:49:55 +0530

    update files

--- server/index.js
+++ server/index.js
@@ -1,69 +1,65 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import _ from 'lodash';
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-mongoose.connect("mongodb+srv://heymayank12:heymayank12@cluster0.1meyy.mongodb.net/blogDB", {useNewUrlParser:true});
+const PORT = process.env.PORT || 3001;
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.1meyy.mongodb.net/blogDB`,
+  { useNewUrlParser: true }
+);
 
 const blogSchema = {
-    postTitle:String,
-    postBody:String
+  postTitle: String,
+  postBody: String,
 };
 
-const Blog = mongoose.model("Blog",blogSchema);
+const Blog = mongoose.model("Blog", blogSchema);
 
-app.get("/",(req,res) => {
-    Blog.find()
-    .then(blogs => res.json(blogs))
-    .catch(err => res.status(400).json(err));
+app.get("/", (req, res) => {
+  Blog.find()
+    .then((blogs) => res.json(blogs))
+    .catch((err) => res.status(400).json(err));
 });
 
 app.post("/compose", (req, res) => {
- const userPost = new Blog ({
-     postTitle:req.body.title,
-     postBody:req.body.post
- });
+  const userPost = new Blog({
+    postTitle: req.body.title,
+    postBody: req.body.post,
+  });
 
-userPost.save()
-.then(() => console.log("Added item successfully"))
-.catch((err) => console.log(err));
+  userPost
+    .save()
+    .then(() => console.log("Added item successfully"))
+    .catch((err) => console.log(err));
 });
 
-
-app.post("/delete",(req,res) => {
-    Blog.deleteOne({_id:req.body._id}, (err) => {
-        if(err) console.log(err)
-        else console.log("Delete Item successfully");
-    })
+app.post("/delete", (req, res) => {
+  Blog.deleteOne({ _id: req.body._id }, (err) => {
+    if (err) console.log(err);
+    else console.log("Delete Item successfully");
+  });
 });
 
-app.post("/update", (req,res) => {
- console.log(req.body);
- Blog.updateOne({_id:req.body._id},{postTitle:req.body.postTitle,postBody:req.body.postBody},(err) => {
-     if(err) console.log(err);
-     else console.log("Updated Successfully");
- })
+app.post("/update", (req, res) => {
+  console.log(req.body);
+  Blog.updateOne(
+    { _id: req.body._id },
+    { postTitle: req.body.postTitle, postBody: req.body.postBody },
+    (err) => {
+      if (err) console.log(err);
+      else console.log("Updated Successfully");
+    }
+  );
 });
 
-app.listen(3001 , () => {
-    console.log("server up and running at 3001");
-})
+app.listen(PORT, () => {
+  console.log("server up and running at 3001");
+});
